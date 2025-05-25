7b64b5c9d93fd7b248e9768205cb2a12541c273c
MalkaYehudit <154057833+MalkaYehudit@users.noreply.github.com>
2025-05-25 12:30:49 +0300

    Time: 0 ms (100%), Space: 18.2 MB (41.25%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        
+        for i in range(n):
+            h = n - i
+            if citations[i] >= h:
+                return h
+        return 0
