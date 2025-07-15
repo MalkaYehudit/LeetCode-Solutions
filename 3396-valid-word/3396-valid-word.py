373d563d0c37f2a3fe217e94551bf94bf7386aa1
MalkaYehudit <154057833+MalkaYehudit@users.noreply.github.com>
2025-07-15 17:56:23 +0300

    Time: 0 ms (100%), Space: 12.3 MB (78.57%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        if len(word) < 3:
+            return False
+
+        vowels = set('aeiouAEIOU')
+        has_vowel = False
+        has_consonant = False
+
+        for ch in word:
+            if not (ch.isalpha() or ch.isdigit()):
+                return False
+
+            if ch.isalpha():
+                if ch in vowels:
+                    has_vowel = True
+                else:
+                    has_consonant = True
+
+        return has_vowel and has_consonant
